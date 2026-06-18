d52859115eed22273267b378d17db3fc967246af
Nerd-Nikhil <nikhilkumarsingh1366@gmail.com>
2026-06-19 00:46:15 +0530

    fixed deployment issue

--- src/index.js
+++ src/index.js
@@ -3,8 +3,7 @@ const configApp = require("./settings/config");
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 
-process.env.NODE_ENV = process.env.NODE_ENV || "development";
-const envFile = `.env.${process.env.NODE_ENV}`;
+const envFile = process.env.NODE_ENV;
 
 dotenv.config({path:envFile})
 
@@ -19,8 +18,7 @@ configApp(app);
 
 async function bootstrap(){
     try{
-        await mongoose.connect(process.env.DATABASE_URL,
-            {dbName:process.env.DATABASE_NAME});
+        await mongoose.connect(process.env.DATABASE_URL);
         console.log("connected to mongoDB");
         app.listen(port,()=>{
             console.log(`application listening at port: ${port}`);
